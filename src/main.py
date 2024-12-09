ef2ba5ad9c38c69a1ab7c36d2ed21e2345b1b0fd
Lucabased.xyz <kontakt@lucabased.xyz>
2024-12-09 20:10:56 +0100

    me

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 from dotenv import load_dotenv
-from src.assets import logo_ascii
+from assets import logo_ascii
 print(logo_ascii)
 # Load environment variables from .env file
 load_dotenv()
@@ -13,5 +13,14 @@ if not SHODAN_API_KEY:
     print("[!] SHODAN_API_KEY is not set in the environment or .env file.")
     exit(1)
     
-from src.shodan_utils import get_vuln
-print(get_vuln("CVE-2024-23897", SHODAN_API_KEY))
+# Await input for CVE Code
+import re
+cve_code_input = ""
+cve_regex = r'^CVE-\d{4}-\d{4,}$'
+while not bool(re.match(cve_regex, cve_code_input)):
+    cve_code_input = input("Input your CVE-Code: ").strip(" ")
+print(f"\n[*] Searching for {cve_code_input}")
+
+# CVE-2024-23897 
+from shodan_utils import get_vuln
+print(get_vuln(cve_code_input, SHODAN_API_KEY))
